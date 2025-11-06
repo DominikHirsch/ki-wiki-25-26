64ece1c7cc84a5275ce6b0dba9908969f1233f46
UniCat22Md <UniCat22Md@users.noreply.github.com>
2025-11-06 16:38:28 +0000

    deploy: 581adf8fc581a6f0f0e69509e456cdbba8f3c96c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Seite 1",
         "excerpt":"Dies ist Seite 1 im Wiki. Seite 1 hat keine weiteren Unterseiten.   ","categories": [],
         "tags": [],
-        "url": "/jekyll-wiki-template/seite1/",
+        "url": "/ki-wiki-25-26/seite1/",
         "teaser": null
       },{
         "title": "Seite 2",
         "excerpt":"Dies ist Seite 2 im Wiki.  Sie hat zwei Unterseiten.  ","categories": [],
         "tags": [],
-        "url": "/jekyll-wiki-template/seite2/",
+        "url": "/ki-wiki-25-26/seite2/",
         "teaser": null
       },{
         "title": "Unterseite 1",
         "excerpt":"Das ist die erste Unterseite von Seite 2.  ","categories": [],
         "tags": [],
-        "url": "/jekyll-wiki-template/seite2/sub1/",
+        "url": "/ki-wiki-25-26/seite2/sub1/",
         "teaser": null
       },{
         "title": "Unterseite 2",
         "excerpt":"Das ist die zweite Unterseite von Seite 2.  ","categories": [],
         "tags": [],
-        "url": "/jekyll-wiki-template/seite2/sub2/",
+        "url": "/ki-wiki-25-26/seite2/sub2/",
         "teaser": null
       },{
         "title": "Soziologische Perspektive",
-        "excerpt":"Test Überschrift  ","categories": [],
+        "excerpt":"Test Überschrift   ","categories": [],
         "tags": [],
-        "url": "/jekyll-wiki-template/Soziologische_Perspektive/",
+        "url": "/ki-wiki-25-26/Soziologische_Perspektive/",
         "teaser": null
       },]
